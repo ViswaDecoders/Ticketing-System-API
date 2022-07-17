40780651b9464540e18ca991709c2f08e5f123de
Viswanath <alapativiswanath1@gmail.com>
2022-07-17 14:54:37 +0530

    Database connection added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
+const CONNECTION_URL = process.env.MONGODB_URI;
+mongoose.connect(CONNECTION_URL);
+const database = mongoose.connection
 
 const app = express();
-
 app.use(express.json());
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 
 app.listen(3000, () => {
     console.log(`Server Started at ${5000}`)
