5249f6e478497e7a15a21f2358b403fcc137e7d4
Viswanath <alapativiswanath1@gmail.com>
2022-07-17 19:45:18 +0530

    Created structure of model

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const Model = require('../models/model')
 const secretKey = "SeCrEtKeY";
 module.exports = router;
 
